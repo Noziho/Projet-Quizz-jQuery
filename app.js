9e386abed94b4c5ae3addbdce2ddd64a59b3a701
Noah2211 <noah.decroix3@gmail.com>
2021-12-22 15:51:10 +0100

    found condition

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let questions = [
     "Combien font 2 + 2 x 4 ?",
     "Combien il y a d'habitant en France ?",
     "Quel est l'anime le plus aimé du monde ? ",
-    "Combien mesure la plus grande tour du monde ?",
+    "Quelle est la plus grande tour du monde ?",
     "Combien de saison possède l'anime 'Naruto' (en tout) ? ",
     "Quel est l'artiste français à occuper le plus souvent la première place spotify ?",
     "Parmis ses vidéo laquelle à été la plus visionnée de YouTube ?",
@@ -34,7 +34,51 @@ $('#startGame').click(function () {
     $('#startGame').html('Suivant');
     $('#welcomeText').html(questions[counter].toString());
     $('#reponse').html(reponse[counter].toString() + '<br>');
-    counter++;
     inputResponse.show();
+    inputResponse.html('')
 
+    if(questions[0] && inputResponse.get(0).value === "10") {
+        point++;
+        console.log(point);
+    }
+
+    if(questions[1] && inputResponse.get(0).value === "67 millions" ||inputResponse.get(0).value === "67" ||inputResponse.get(0).value === "67millions") {
+        point++;
+        console.log(point);
+    }
+    if(questions[2] && inputResponse.get(0).value === "One piece" ||inputResponse.get(0).value === "one piece") {
+        point++;
+        console.log(point);
+    }
+    if(questions[3] && inputResponse.get(0).value === "Burj Khalifa" ||inputResponse.get(0).value === "burj khalifa") {
+        point++;
+        console.log(point);
+    }
+    if(questions[4] && inputResponse.get(0).value === "30" ||inputResponse.get(0).value === "30 saisons") {
+        point++;
+        console.log(point);
+    }
+    if(questions[5] && inputResponse.get(0).value === "Ninho" ||inputResponse.get(0).value === "ninho") {
+        point++;
+        console.log(point);
+    }
+
+    if(questions[6] && inputResponse.get(0).value === "Baby Shark" ||inputResponse.get(0).value === "baby shark" ||inputResponse.get(0).value === "Babyshark" ||inputResponse.get(0).value === "BabyShark" ||inputResponse.get(0).value === "babyshark") {
+        point++;
+        console.log(point);
+    }
+    if(questions[7] && inputResponse.get(0).value === "Logitech G502" ||inputResponse.get(0).value ==="logitech G502" ||inputResponse.get(0).value === "logitech g502") {
+        point++;
+        console.log(point);
+    }
+    if(questions[8] && inputResponse.get(0).value === "Minecraft" ||inputResponse.get(0).value === "minecraft") {
+        point++;
+        console.log(point);
+    }
+    if(questions[9] && inputResponse.get(0).value === "Java" ||inputResponse.get(0).value === "java") {
+        point++;
+        console.log(point);
+    }
+
+    counter++;
 })
